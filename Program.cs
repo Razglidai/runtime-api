8c42077d563c3c823d1f305cc2c82f8785c54e33
Paul <turboskomorokh@gmail.com>
2025-01-17 07:54:48 +0300

    продолжение отката

--- Program.cs
+++ Program.cs
@@ -5,18 +5,39 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
 services.AddControllers();
 
 services.AddTransient<ExecutorStorage>();
 services.AddScoped<IRuntimeExecutor, PythonExecutor>();
 services.AddScoped<IRuntimeExecutor, CExecutor>();
 
+
+
+
+// Радиоактивная херня
+services.AddSwaggerGen();
+
+
+
+
+
 // Собираем приложение
 var app = builder.Build();
 
+
+
+
+
+
+// Ща сломается
 app.UseSwagger();
 app.UseSwaggerUI();
+
+
+
+
+
+
 
 app.MapControllers();
 
