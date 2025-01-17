f6799605ac84008e64e20b46eae56b034842a270
turboskomorokh <turboskomorokh@gmail.com>
2025-01-18 00:55:42 +0300

    инициализация сервисов

--- Program.cs
+++ Program.cs
@@ -8,14 +8,17 @@ services.AddEndpointsApiExplorer();
 services.AddControllers();
 
 services.AddTransient<ExecutorStorage>();
+services.AddScoped<IRuntimeExecutor, DlangExecutor>();
 services.AddScoped<IRuntimeExecutor, ClangExecutor>();
 services.AddScoped<IRuntimeExecutor, CPlusPlusExecutor>();
 services.AddScoped<IRuntimeExecutor, RustExecutor>();
-services.AddScoped<IRuntimeExecutor, DlangExecutor>();
 
-services.AddScoped<IRuntimeExecutor, PythonExecutor>();
+services.AddScoped<IRuntimeExecutor, BasicExecutor>();
+services.AddScoped<IRuntimeExecutor, JavaScriptExecutor>();
 services.AddScoped<IRuntimeExecutor, LuaExecutor>();
 services.AddScoped<IRuntimeExecutor, PerlExecutor>();
+services.AddScoped<IRuntimeExecutor, PythonExecutor>();
+services.AddScoped<IRuntimeExecutor, RubyExecutor>();
 
 if (builder.Environment.IsDevelopment())
 {
